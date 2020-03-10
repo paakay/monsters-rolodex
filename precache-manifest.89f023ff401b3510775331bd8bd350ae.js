f4aa454faa07e22c33a60d26ed4c01dea163ff0d
Kwessi Annor <kayannor@gmail.com>
2020-03-10 16:07:50 -0400

    Updates

---
+++ precache-manifest.89f023ff401b3510775331bd8bd350ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b448cf99cf4c839af0c04f247d227ce6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b7ece45782fa21c4ed1e",
+    "url": "/monsters-rolodex/static/css/main.85f704b6.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ece45782fa21c4ed1e",
+    "url": "/monsters-rolodex/static/js/main.a5fb3f31.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
