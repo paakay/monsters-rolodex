96f5c03f6405d03308470338692fc4f169002133
Kwessi Annor <kayannor@gmail.com>
2020-03-10 11:21:42 -0400

    Make SearchBox component accessible

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -5,11 +5,18 @@ import './search-box.styles.css'
 const SearchBox = ({ placeholder, handleChange }) => {
   return (
     <div>
+      <div className="label-container">
+        <label htmlFor="search-box">Search monsters: </label>
+        <span id="search-box-hint" className="input-hint">Example: Patricia</span>
+      </div>
       <input
-        className="search-box" 
+        id="search-box"
+        name="search-box"
         type="search"
-        placeholder={placeholder}
+        className="search-box" 
+        placeholder={placeholder} 
         onChange={handleChange}
+        aria-describedby="search-box-hint"
       />
     </div>
   )
