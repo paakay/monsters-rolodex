6d430ee3b920a0fe6bc5ee0dd2ef92164943a43b
Kwessi Annor <kayannor@gmail.com>
2020-03-11 08:14:04 -0400

    Updates

---
+++ precache-manifest.807ca8339b029e47ce249d8f17720127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d860ff93c76a912938263ed9a2bc4773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d188c3241bfce87b414",
+    "url": "/static/css/main.85f704b6.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d188c3241bfce87b414",
+    "url": "/static/js/main.2d1d2b70.chunk.js"
+  },
+  {
+    "revision": "a53c56bcba93a6115910",
+    "url": "/static/js/runtime-main.4595a359.js"
+  }
+]);
