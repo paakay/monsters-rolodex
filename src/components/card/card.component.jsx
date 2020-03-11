fa1c63c50c16c26e1ed0d9f422056f1327964c50
Kwessi Annor <kayannor@gmail.com>
2020-03-11 08:07:42 -0400

    Fix Card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,11 +4,11 @@ import './card.styles.css'
 
 const Card = (props) => {
   return (
-    <figure className="card-container">
+    <div className="card-container">
       <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt={`${props.monster.name} monster`} title={`${props.monster.username} from ${props.monster.address.city}`} />
       <h2>{props.monster.name}</h2>
-      <>{props.monster.email}</>
-    </figure>
+      <p>{props.monster.email}</p>
+    </div>
   )
 }
 
